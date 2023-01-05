83c632339ed86c87584918e8f7178c0c12759e6c
Mohib Qureshi <qureshimohib819@gmail.com>
2023-01-05 20:26:39 +0500

    adding autosearch option

---
+++ src/assets/data/items.js
@@ -0,0 +1,22 @@
+export const items = [
+    {
+      id: 0,
+      name: 'Cobol'
+    },
+    {
+      id: 1,
+      name: 'JavaScript'
+    },
+    {
+      id: 2,
+      name: 'Basic'
+    },
+    {
+      id: 3,
+      name: 'PHP'
+    },
+    {
+id:33,
+      name: 'Java'
+    }
+  ]
