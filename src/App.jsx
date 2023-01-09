7f40b9981ae7fda0ea655d5a3048575b7bb8d8ee
YamalAli770 <yamalali770@gmail.com>
2023-01-09 23:16:25 +0500

    created candidate profile creation page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import BrowseCandidates from "./components/BrowseCandidates"
 import BrowseCompanies from "./components/BrowseCompanies"
 import CandidateProfile from "./components/CandidateProfile"
 import CompanyProfile from "./components/CompanyProfile"
+import CreateCandidateProfile from "./components/CreateCandidateProfile"
 import FindJob from "./components/FindJob"
 import HomePageComponents from "./components/HomePageComponents"
 import Login from "./components/Login"
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/createprofile" element={<CreateCandidateProfile />} />
         <Route element={<WithNavAndFooter />}>
           <Route path="/" element={<HomePageComponents />} />
           <Route path="/candidates" element={<BrowseCandidates />} />
--- src/components/BrowseCandidates.jsx
+++ src/components/BrowseCandidates.jsx
@@ -3,42 +3,42 @@ import CdAndCpHeader from './CdAndCpHeader'
 import CandidateCard from './CandidateCard'
 import Pagination from './Pagination'
 import ReactLoading from "react-loading"
-import {  useLocation,Link } from 'react-router-dom';
-import { useDispatch,useSelector } from 'react-redux'
+import { useLocation, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
-import { getAllCandidate} from '../actions/user.js'
+import { getAllCandidate } from '../actions/user.js'
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
 const BrowseCandidates = () => {
-  const dispatch=useDispatch()
-  const {candidates,isLoading}=useSelector(state=>state.centralStore)
-  const query=useQuery()
-  const page=query.get('page') || 1
-  
-  useEffect(()=>{
-  dispatch(getAllCandidate(page))
-  },[page])
+  const dispatch = useDispatch()
+  const { candidates, isLoading } = useSelector(state => state.centralStore)
+  const query = useQuery()
+  const page = query.get('page') || 1
 
- if(isLoading){
-  return   <ReactLoading color='black' type="spin" height={337} width={115} />
- }
+  useEffect(() => {
+    dispatch(getAllCandidate(page))
+  }, [page])
+
+  if (isLoading) {
+    return <ReactLoading color='black' type="spin" height={337} width={115} />
+  }
 
   return (
     <div className="browse-candidates-container">
-        <CdAndCpHeader type="candidates" />
-        <p className="candidates-shown">Showing <span>41-60</span> of <span>944</span> jobs</p>
-        <section className="browse-candidates-grid">
-{candidates.map((i)=>{
- return <Link   to={`${i.userId}`}style={{textDecoration: 'none' }} ><CandidateCard profile={i}/></Link>
-})
+      <CdAndCpHeader type="candidates" />
+      <p className="candidates-shown">Showing <span>41-60</span> of <span>944</span> jobs</p>
+      <section className="browse-candidates-grid">
+        {candidates.map((i) => {
+          return <Link to={`${i.userId}`} style={{ textDecoration: 'none' }} ><CandidateCard profile={i} /></Link>
+        })
 
- } 
-     
-        </section>
-        <Pagination  number={5} type={"candidates"}/>
+        }
+
+      </section>
+      <Pagination number={5} type={"candidates"} />
     </div>
   )
 }
