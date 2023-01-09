3fd27e4dbca3cf803e9da675893df1daee8f2907
Mohib Qureshi <qureshimohib819@gmail.com>
2023-01-09 11:03:36 +0500

    some changes

--- src/components/BrowseCompanies.jsx
+++ src/components/BrowseCompanies.jsx
@@ -4,23 +4,36 @@ import CdAndCpHeader from './CdAndCpHeader'
 import Filter from './Filter'
 import CompanyCard from './CompanyCard'
 import { useDispatch,useSelector} from 'react-redux'
+import {  useLocation } from 'react-router-dom';
 import { useEffect } from 'react'
 import { getAllCompany } from '../actions/company'
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 const BrowseCompanies = () => {
 
  const dispatch=useDispatch()
  const {companies}=useSelector(state=>state.centralStore)
+const query=useQuery()
+const page=query.get('page')||1
 
  useEffect(()=>{
-dispatch(getAllCompany(1))
- },[])
+dispatch(getAllCompany(page))
+ },[page])
+
+//  if()
     return (
     <div className='browse-companies-container'>
         <CdAndCpHeader type="companies" />
         <section className="filter-cards-grid">
             <Filter />
-            <CardsGrid cardType={<CompanyCard />} qty={8} />
+
+            <CardsGrid cardType=
+            {<CompanyCard />}
+            qty={8} /> 
+         
+      
         </section>
     </div>
   )
