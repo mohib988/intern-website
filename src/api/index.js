a993cf8cde803e60bcfea7048dd6d3155f87708d
Mohib Qureshi <qureshimohib819@gmail.com>
2023-01-18 20:34:12 +0500

    some middleware

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,15 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+  
+    return req;
+  });
+  
+
 
 export const fetchPostsBySearch = (query) => API.get(`/jobPost/search/query?title=${query}`);
 
